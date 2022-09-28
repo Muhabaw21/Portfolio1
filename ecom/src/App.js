4dbc0cb4ce950b5b33570a371b8145c14904a562
Alemayehu <alluwa2127@gmail.com>
2022-09-28 16:10:50 +0300

    Ecommerce

---
+++ ecom/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div >
+      <header className="brand" >
+  <a href = "/">eCommerce</a>
+      </header>
+      <main>
+      list product
+      </main>
+    </div>
+  );
+}
+
+export default App;
